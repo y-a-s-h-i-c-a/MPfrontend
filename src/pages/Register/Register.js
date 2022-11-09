8cea8f3756a1c4a77fc055df778d5994358fd276
y-a-s-h-i-c-a <yashica1501@gmail.com>
2022-11-09 22:41:49 +0530

    commit

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -12,7 +12,7 @@ function Register() {
     async function registerUser(event) {
         event.preventDefault();
         
-        const response = await fetch ("http://localhost:3000/api/register",{
+        const response = await fetch ("https://marketplacee.herokuapp.com/api/register",{
             method:"POST",
             headers:{
                 "Content-Type": "application/json",
